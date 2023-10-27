0cb0a2ef2de6bc85d81389b37f14f931f9f1cb59
Raj Kumar Sahu <rajkumarsahu2002@gmail.com>
2023-10-27 10:35:20 +0530

    Modified some code

--- problem2.js
+++ problem2.js
@@ -3,8 +3,9 @@ const fs = require('fs').promises;
 async function readAndConvertToUpperCase(inputFileName) {
     const data = await fs.readFile(inputFileName, 'utf-8');
     const uppercasedData = data.toUpperCase();
-    await fs.writeFile('/home/raj/async_js_exercises/uppercase.txt', uppercasedData);
-    return '/home/raj/async_js_exercises/uppercase.txt';
+    await fs.writeFile('../uppercase.txt', uppercasedData);
+    await fs.appendFile('../filenames.txt', '../uppercase.txt\n');
+    return '../uppercase.txt';
 }
 
 async function convertToLowerCaseAndSplit(inputFileName) {
@@ -12,18 +13,18 @@ async function convertToLowerCaseAndSplit(inputFileName) {
     const lowercasedData = data.toLowerCase();
     const sentences = lowercasedData.split(/[.!?]/).filter(sentence => sentence.trim() !== '');
     const newFiles = await Promise.all(sentences.map((sentence, index) => {
-        const fileName = `sentence${index + 1}.txt`;
+        const fileName = `sentence${index + 1}.txt`; 
         return fs.writeFile(fileName, sentence.trim()).then(() => fileName);
     }));
-    await fs.appendFile('/home/raj/async_js_exercises/filenames.txt', newFiles.join('\n') + '\n');
+    await fs.appendFile('../filenames.txt', newFiles.join('\n') + '\n');
     return newFiles;
 }
 
 async function sortAndWrite(files) {
     const contents = await Promise.all(files.map(file => fs.readFile(file, 'utf-8')));
     contents.sort();
-    await fs.writeFile('/home/raj/async_js_exercises/sorted.txt', contents.join('\n'));
-    await fs.appendFile('/home/raj/async_js_exercises/filenames.txt', '/home/raj/async_js_exercises/sorted.txt\n');
+    await fs.writeFile('../sorted.txt', contents.join('\n'));
+    await fs.appendFile('../filenames.txt', '../sorted.txt\n');
     return 'sorted.txt';
 }
 
